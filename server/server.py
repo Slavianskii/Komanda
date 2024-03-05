f90af4a4e14b4c80d36b8fe2b53bf532d7cdca05
Slava <slawa34569@gmail.com>
2024-03-05 17:38:45 +0300

    Добавлены операции калькулятора

--- server/server.py
+++ server/server.py
@@ -7,9 +7,30 @@ def calculate():
     data = request.get_json()
     arg1 = data['arg1']
     arg2 = data['arg2']
+    operation = data['operation']
 
+    # Проверяем, что аргументы числа
+    try:
+        arg1 = float(arg1)
+        arg2 = float(arg2)
+    except ValueError:
+        return jsonify({'error': 'Аргументы должны быть числами'})
 
-    return jsonify({'result': None})
+    if operation == '+':
+        result = arg1 + arg2
+    elif operation == '-':
+        result = arg1 - arg2
+    elif operation == '*':
+        result = arg1 * arg2
+    elif operation == '/':
+        if arg2 == 0:
+            return jsonify({'error': 'Деление на ноль невозможно'})
+        result = arg1 / arg2
+    else:
+        return jsonify({'error': 'Неподдерживаемая операция'})
+
+    return jsonify({'result': result})
+
 
 if __name__ == '__main__':
     app.run(debug=False)
